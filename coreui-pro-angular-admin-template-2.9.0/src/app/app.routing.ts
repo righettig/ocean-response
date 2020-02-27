d0f7255689e7eeae5c0b41e1913af3c00cdcea40
Giacomo Righetti <kirkrulez83@gmail.com>
2020-02-27 15:36:35 +0000

    Changed default module & further code cleanup

--- coreui-pro-angular-admin-template-2.9.0/src/app/app.routing.ts
+++ coreui-pro-angular-admin-template-2.9.0/src/app/app.routing.ts
@@ -5,14 +5,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { DefaultLayoutComponent } from './containers';
 
 import { P404Component } from './views/error/404.component';
-// import { P500Component } from './views/error/500.component';
-// import { LoginComponent } from './views/login/login.component';
-// import { RegisterComponent } from './views/register/register.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'dashboard',
+    redirectTo: 'emergencies-maps', // dashboard
     pathMatch: 'full',
   },
   {
@@ -22,27 +19,6 @@ export const routes: Routes = [
       title: 'Page 404'
     }
   },
-  // {
-  //   path: '500',
-  //   component: P500Component,
-  //   data: {
-  //     title: 'Page 500'
-  //   }
-  // },
-  // {
-  //   path: 'login',
-  //   component: LoginComponent,
-  //   data: {
-  //     title: 'Login Page'
-  //   }
-  // },
-  // {
-  //   path: 'register',
-  //   component: RegisterComponent,
-  //   data: {
-  //     title: 'Register Page'
-  //   }
-  // },
   {
     path: '',
     component: DefaultLayoutComponent,
@@ -54,22 +30,10 @@ export const routes: Routes = [
         path: 'base',
         loadChildren: () => import('./views/base/base.module').then(m => m.BaseModule)
       },
-      // {
-      //   path: 'buttons',
-      //   loadChildren: () => import('./views/buttons/buttons.module').then(m => m.ButtonsModule)
-      // },
-      // {
-      //   path: 'charts',
-      //   loadChildren: () => import('./views/chartjs/chartjs.module').then(m => m.ChartJSModule)
-      // },
       {
         path: 'dashboard',
         loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
       },
-      // {
-      //   path: 'editors',
-      //   loadChildren: () => import('./views/editors/editors.module').then(m => m.EditorsModule)
-      // },
       {
         path: 'forms',
         loadChildren: () => import('./views/forms/forms.module').then(m => m.FormsModule)
@@ -78,34 +42,10 @@ export const routes: Routes = [
         path: 'emergencies-maps',
         loadChildren: () => import('./views/google-maps/google-maps.module').then(m => m.GoogleMapsModule)
       },
-      // {
-      //   path: 'icons',
-      //   loadChildren: () => import('./views/icons/icons.module').then(m => m.IconsModule)
-      // },
-      // {
-      //   path: 'notifications',
-      //   loadChildren: () => import('./views/notifications/notifications.module').then(m => m.NotificationsModule)
-      // },
-      // {
-      //   path: 'plugins',
-      //   loadChildren: () => import('./views/plugins/plugins.module').then(m => m.PluginsModule)
-      // },
       {
         path: 'tables',
         loadChildren: () => import('./views/tables/tables.module').then(m => m.TablesModule)
-      },
-      // {
-      //   path: 'theme',
-      //   loadChildren: () => import('./views/theme/theme.module').then(m => m.ThemeModule)
-      // },
-      // {
-      //   path: 'apps',
-      //   loadChildren: () => import('./views/apps/apps.module').then(m => m.AppsModule)
-      // },
-      // {
-      //   path: 'widgets',
-      //   loadChildren: () => import('./views/widgets/widgets.module').then(m => m.WidgetsModule)
-      // }
+      }
     ]
   },
   { path: '**', component: P404Component }
